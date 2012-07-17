56b3b2dd1d506b898d9d033362993d87f625dc69
Oleg Kalnichevski <olegk@apache.org>
2012-07-17 15:28:28 +0000

    Temporary workaround for HTTPASYNC-21
    git-svn-id: https://svn.apache.org/repos/asf/httpcomponents/httpasyncclient/trunk@1362541 13f79535-47bb-0310-9956-ffa450edef68

--- httpasyncclient/src/test/java/org/apache/http/nio/client/integration/TestHttpAsyncPrematureTermination.java
+++ httpasyncclient/src/test/java/org/apache/http/nio/client/integration/TestHttpAsyncPrematureTermination.java
@@ -65,7 +65,6 @@ import org.apache.http.protocol.HttpContext;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class TestHttpAsyncPrematureTermination extends HttpAsyncTestBase {
@@ -115,7 +114,7 @@ public class TestHttpAsyncPrematureTermination extends HttpAsyncTestBase {
         return target;
     }
 
-    @Test @Ignore
+    @Test
     public void testConnectionTerminatedProcessingRequest() throws Exception {
         HttpAsyncRequestHandlerRegistry registry = new HttpAsyncRequestHandlerRegistry();
         registry.register("*", new HttpAsyncRequestHandler<HttpRequest>() {
@@ -125,7 +124,6 @@ public class TestHttpAsyncPrematureTermination extends HttpAsyncTestBase {
                     final HttpContext context) throws HttpException, IOException {
                 HttpConnection conn = (HttpConnection) context.getAttribute(
                         ExecutionContext.HTTP_CONNECTION);
-                System.out.println("Boom!!!!");
                 conn.shutdown();
                 return new BasicAsyncRequestConsumer();
             }
@@ -165,7 +163,7 @@ public class TestHttpAsyncPrematureTermination extends HttpAsyncTestBase {
         Assert.assertTrue(latch.await(5, TimeUnit.SECONDS));
     }
 
-    @Test @Ignore
+    @Test
     public void testConnectionTerminatedHandlingRequest() throws Exception {
         HttpAsyncRequestHandlerRegistry registry = new HttpAsyncRequestHandlerRegistry();
         registry.register("*", new HttpAsyncRequestHandler<HttpRequest>() {
@@ -182,7 +180,6 @@ public class TestHttpAsyncPrematureTermination extends HttpAsyncTestBase {
                     final HttpContext context) throws HttpException, IOException {
                 HttpConnection conn = (HttpConnection) context.getAttribute(
                         ExecutionContext.HTTP_CONNECTION);
-                System.out.println("Boom!!!!");
                 conn.shutdown();
                 HttpResponse response = httpExchange.getResponse();
                 response.setEntity(new NStringEntity("all is well", ContentType.TEXT_PLAIN));
