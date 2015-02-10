e0d3d5cf9cc86ceced315426c901d45bd41a8d5a
Oleg Kalnichevski <olegk@apache.org>
2015-02-10 12:07:51 +0000

    HTTPASYNC-86: fixed a race condition upon connection lease from the connection pool: in very rare  circumstances the main execution thread can get jammed for so long that the I/O dispatch thread succeeds in completing the request and releasing the connection while the main still is performing connection validation
    git-svn-id: https://svn.apache.org/repos/asf/httpcomponents/httpasyncclient/branches/4.0.x@1658693 13f79535-47bb-0310-9956-ffa450edef68

--- httpasyncclient/src/main/java/org/apache/http/impl/nio/client/DefaultClientExchangeHandlerImpl.java
+++ httpasyncclient/src/main/java/org/apache/http/impl/nio/client/DefaultClientExchangeHandlerImpl.java
@@ -304,7 +304,7 @@ class DefaultClientExchangeHandlerImpl<T>
 
             managedConn.getContext().setAttribute(HttpAsyncRequestExecutor.HTTP_HANDLER, this);
             managedConn.requestOutput();
-            if (!managedConn.isOpen()) {
+            if (managedConn.isStale()) {
                 failed(new ConnectionClosedException("Connection closed"));
             }
         } catch (final RuntimeException runex) {
